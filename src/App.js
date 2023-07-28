0d3dc3ac3cb0f7815a93cb070096263f45b57d9c
MrRinkukumar <rinkukumarsaini9799118495@gmail.com>
2023-07-28 12:25:19 +0530

    upgrading the layout and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,61 +1,100 @@
 import "./App.css";
-import About from "./components/About";
-import Navbar from "./components/Navbar";
 
 import React, { useState } from "react";
-import Alert from "./components/Alert";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Textform from "./components/Textform";
-import Footer from "./components/Footer";
 
 function App() {
-  const [mode, setmode] = useState("light");
-  const toggleMode = () => {
-    if (mode === "light") {
-      setmode("dark");
-      document.body.style.backgroundColor = "#121212";
-      showAlert("Dark mode Enabled", "success");
-    } else {
-      setmode("light");
-      document.body.style.backgroundColor = "white";
-      showAlert("light mode Enabled", "success");
-    }
-  };
-  const [alert, setAlert] = useState(null);
-  const showAlert = (message, type) => {
-    setAlert({
-      type: type,
-      msg: message,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 2000);
-  };
   return (
     <>
-      <BrowserRouter>
-        <Navbar title="TxtAnalyser" mode={mode} toggleMode={toggleMode} />
-        <div className="App-header">
-          <Alert alert={alert} />
-
-          <div className="container">
-            <Routes>
-              <Route
-                exact
-                path="/"
-                element={
-                  <Textform />
-                }
-              />
-              <Route exact path="/about" element={<About />} />
-            </Routes>
-          </div>
-        </div>
-        <strong><hr /></strong>
-        <Footer />
-      </BrowserRouter>
+      <nav className="navbar">
+        <p>Text analyser</p>{" "}
+      </nav>
+      <TextForm />
     </>
   );
 }
 
 export default App;
+
+const TextForm = (props) => {
+  const UpperCase = () => {
+    // console.log('upper hitted')
+    let newText = Text.toUpperCase();
+    setText(newText);
+  };
+  const lowercase = () => {
+    let newText = Text.toLowerCase();
+    setText(newText);
+  };
+  const Clear = () => {
+    let newText = "";
+    setText(newText);
+  };
+  const handleCopy = () => {
+    var text = document.getElementById("Textform");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const RemoveExtraaSpaces = () => {
+    let newtext = Text.split(/[ ]+/);
+    setText(newtext.join(" "));
+  };
+
+  const OnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [Text, setText] = useState("");
+  return (
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={Text}
+            onChange={OnChange}
+           placeholder="Enter your text here"
+            id="Textform"
+            rows="8"
+          ></textarea>
+          <button className=" buttons" onClick={UpperCase}>
+            to Upper case
+          </button>
+          <button className=" buttons" onClick={lowercase}>
+            {" "}
+            Lower case
+          </button>
+          <button className=" buttons" onClick={handleCopy}>
+            copy to clipboard
+          </button>
+          <button className=" buttons" onClick={Clear}>
+            Clear
+          </button>
+          <button className=" buttons" onClick={RemoveExtraaSpaces}>
+            Remove Extra Spaces
+          </button>
+        </div>
+      </div>
+      <div className="container text-black text-opacity-75">
+        <h1 className="mainHeading">Text-Summary</h1>
+        <div className="container text-opacity-50">
+          <p class="card-body d-flex">
+            <p className="container subHeading ">
+              Word : <span>{Text.split(" ").length - 1}</span>
+            </p>
+            <p className="container subHeading ">
+              Character: <span>{Text.length}</span>{" "}
+            </p>
+            <p className="container subHeading ">
+              Reading time : <span>{0.008 * Text.split(" ").length}</span>{" "}
+            </p>
+          </p>
+          <hr style={{ width: "100%" }} />
+        </div>
+        <h2 className="mainHeading">Preview</h2>
+        <p className="preview">
+          {Text.length > 0 ? Text : "Enter your text here to preview"}
+        </p>
+      </div>
+    </>
+  );
+};
